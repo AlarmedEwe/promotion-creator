1fdd9eb1410ac2d463c8cb0e982ef505f408b99c
AlarmedEwe <guilherme11do9@gmail.com>
2021-01-18 15:30:11 -0300

    Display more products
    Finally, all the pages are displayed YaY

--- src/backend/XlsxHandle.js
+++ src/backend/XlsxHandle.js
@@ -14,6 +14,17 @@ class XlsxHandle
         this.flyerContent.querySelector(':root').style.setProperty(`--${variable}`,value);
     }
 
+    createProductExhibition(product)
+    {
+        let div = document.createElement('div');
+            div.innerHTML = `<img src="${product.D}" alt="${product.A}"/>
+                <div>
+                    <p>${product.A.toLowerCase()} ${product.B}</p>
+                    <div class="price">R$ ${product.C.toFixed(2).replace('.',',')}</div>
+                </div>`;
+        return div;
+    }
+
     createFlyer(list)
     {
         list.shift();
@@ -22,23 +33,21 @@ class XlsxHandle
         for (let i in list)
         {
             let product = list[i];
+
             if(i < 30)
             {
-                let div = document.createElement('div');
-                div.innerHTML = `<img src="${product.D}" alt="${product.A}"/>
-                    <div>
-                        <p>${product.A.toLowerCase()} ${product.B}</p>
-                        <div class="price">R$ ${product.C.toFixed(2).replace('.',',')}</div>
-                    </div>`;
+                let div = this.createProductExhibition(product);
                 flyerList.appendChild(div);
             } else 
             {
+                let main;
+
                 if (i % 30 == 0)
                 {
                     let page    = document.createElement('main'),
                         header  = document.createElement('div'),
-                        main    = document.createElement('div'),
                         footer  = document.createElement('div');
+                    main    = document.createElement('div');
 
                     header.classList.add('flyerHeader');
                     main.classList.add('flyerMain');
@@ -46,13 +55,19 @@ class XlsxHandle
 
                     header.innerHTML    = '<h2>Visite nosso portal: portal.gramense.com.br</h2>';
                     main.innerHTML      = '<div class="flyerAlert"><p>IMAGENS DE CARÁTER ILUSTRATIVO. RESERVAMOS O DIREITO DE CORRIGIR EVENTUAIS ERROS GRÁFICOS.</p></div>';
+                    main.innerHTML     += '<div class="flyerList"></div>';
                     footer.innerHTML    = '<p>VALIDADE 26 E 27 DE NOVEMBRO DE 2020 OU ENQUANTO DURAR O ESTOQUE.</p>';
 
                     page.appendChild(header);
                     page.appendChild(main);
                     page.appendChild(footer);
                     this.flyerContent.body.appendChild(page);
+                } else {
+                    main = this.flyerContent.querySelector('main:last-child');
                 }
+
+                let div = this.createProductExhibition(product);
+                main.querySelector('.flyerList').appendChild(div);
             }
         }
     }
